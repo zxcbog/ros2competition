f2eae7b05fc7e9329e931806a686228e3c7a4457
1 <1>
2024-12-26 02:55:32 +0700

    Revert "final"
    This reverts commit 17235efcacf837587ab834c8ca69b2c7a87eb1f3.

--- autorace_core_SkufLords/build/autorace_core_SkufLords/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/maxim/git_reps/Robotech/autorace_core_SkufLords/install/autorace_core_SkufLords'
